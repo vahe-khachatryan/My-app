98b493da0e139fa2e71503248c59dbf09b096bd1
vahe-khachatryan <vahe.khachatryann@gmail.com>
2021-03-02 14:38:08 +0400

    Commit

---
+++ src/Components/First.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const First = () => {
+  return (
+    
+    <div>
+      <nav>
+          
+          <ul>
+          
+          <li><a className="nav-link">Courses</a></li>
+          <li><a className="nav-link1" href="#">Popular</a></li>
+          <li><a className="nav-link2" href="#">Favorite</a></li>
+          <li><a className="nav-link3" href="#">New</a></li>
+          
+        </ul>
+      </nav>
+
+      
+    </div>
+  
+        
+  )}
+    
+  
+      
+
+export default First;
---
+++ src/Components/Second.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Second = () => {
+  return (
+  
+ <div className = "background" />
+
+)};
+    
+export default Second;
+
